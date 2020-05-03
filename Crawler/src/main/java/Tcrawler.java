a6af373f531438a47d647b5c3aeb7130e7cda8eb
Nicholas Kory <nmkory@ucdavis.edu>
2020-05-02 22:45:13 -0700

    json working

--- Crawler/src/main/java/Tcrawler.java
+++ Crawler/src/main/java/Tcrawler.java
@@ -8,8 +8,6 @@ import java.util.List;
 public class Tcrawler {
 
     public static void main(String args[]) throws TwitterException, FileNotFoundException, IOException {
-        final Object lock = new Object();
-        final List<Status> tweets = new ArrayList<Status>();
         ConfigurationBuilder cb = new ConfigurationBuilder();
         FileWriter fw = new FileWriter("output.txt");
         BufferedWriter bw = new BufferedWriter(fw);
@@ -18,7 +16,8 @@ public class Tcrawler {
                 .setOAuthConsumerKey("LT4IPByNOJS6RctWDBHN0Favi")
                 .setOAuthConsumerSecret("MYvp76RGm2D8fCZNanEr3aHAt03BBRZEppjmdcOvwZGj9bkAQ7")
                 .setOAuthAccessToken("42011375-PmplF1B0I613j9BWJNG8krXKMDMf9fHCfHH5n5pp6")
-                .setOAuthAccessTokenSecret("NPD0nNhQNdPPFoHrT1xflSDU971fCDi65nPokpY8IPOrg");
+                .setOAuthAccessTokenSecret("NPD0nNhQNdPPFoHrT1xflSDU971fCDi65nPokpY8IPOrg")
+                .setJSONStoreEnabled(true);
 
 
         StatusListener listener = new StatusListener() {
@@ -42,14 +41,8 @@ public class Tcrawler {
 
             @Override
             public void onStatus(Status status) {
-                tweets.add(status);
-                System.out.println(tweets.size() + ":" + status.getText());
-                if (tweets.size() > 100) {
-                    synchronized (lock) {
-                        lock.notify();
-                    }
-                    System.out.println("unlocked");
-                }
+                String json = TwitterObjectFactory.getRawJSON(status);
+                System.out.println(json);
             }
 
             @Override
@@ -62,19 +55,6 @@ public class Tcrawler {
         twitterStream.addListener(listener);
 
         twitterStream.sample("en");
-
-        try {
-            synchronized (lock) {
-                lock.wait();
-            }
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        for (Status tweet : tweets) {
-            System.out.println(tweet);
-        }
-
 
         System.exit(0);
     }
