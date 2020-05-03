681160709f0a795832b934eb4cc43524fd638fa3
Nicholas Kory <nmkory@ucdavis.edu>
2020-05-02 21:17:06 -0700

    Stream working

--- Crawler/src/main/java/Tcrawler.java
+++ Crawler/src/main/java/Tcrawler.java
@@ -1,61 +1,80 @@
 import twitter4j.*;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
+import twitter4j.conf.ConfigurationBuilder;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Tcrawler {
-    //Nick's Twitter account consumer keys - requires login
-    private static final String CONSUMER_KEY = "LT4IPByNOJS6RctWDBHN0Favi";
-    private static final String CONSUMER_SECRET = "MYvp76RGm2D8fCZNanEr3aHAt03BBRZEppjmdcOvwZGj9bkAQ7";
 
-    private static void storeAccessToken(long useId, AccessToken accessToken){
-        //TODO Store tokens safely
-        //accessToken.getToken();
-        //accessToken.getTokenSecret();
-    }
+    public static void main(String args[]) throws TwitterException, FileNotFoundException, IOException {
+        final Object lock = new Object();
+        final List<Status> tweets = new ArrayList<Status>();
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        FileWriter fw = new FileWriter("output.txt");
+        BufferedWriter bw = new BufferedWriter(fw);
 
-    private static Twitter getAccessToken() throws Exception{
-        // The factory instance is re-useable and thread safe.
-        Twitter twitter = TwitterFactory.getSingleton();
-        twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-        RequestToken requestToken = twitter.getOAuthRequestToken();
-        AccessToken accessToken = null;
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        while (null == accessToken) {
-            System.out.println("Open the following URL and grant access to your account:");
-            System.out.println(requestToken.getAuthorizationURL());
-            System.out.print("Enter the PIN(if aviailable) or just hit enter.[PIN]:");
-            String pin = br.readLine();
-            try{
-                if(pin.length() > 0){
-                    accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-                }else{
-                    accessToken = twitter.getOAuthAccessToken();
-                }
-            } catch (TwitterException te) {
-                if(401 == te.getStatusCode()){
-                    System.out.println("Unable to get the access token.");
-                }else{
-                    te.printStackTrace();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey("LT4IPByNOJS6RctWDBHN0Favi")
+                .setOAuthConsumerSecret("MYvp76RGm2D8fCZNanEr3aHAt03BBRZEppjmdcOvwZGj9bkAQ7")
+                .setOAuthAccessToken("42011375-PmplF1B0I613j9BWJNG8krXKMDMf9fHCfHH5n5pp6")
+                .setOAuthAccessTokenSecret("NPD0nNhQNdPPFoHrT1xflSDU971fCDi65nPokpY8IPOrg");
+
+
+        StatusListener listener = new StatusListener() {
+
+            @Override
+            public void onException(Exception e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onDeletionNotice(StatusDeletionNotice arg) {
+            }
+
+            @Override
+            public void onScrubGeo(long userId, long upToStatusId) {
+            }
+
+            @Override
+            public void onStallWarning(StallWarning warning) {
+            }
+
+            @Override
+            public void onStatus(Status status) {
+                tweets.add(status);
+                System.out.println(tweets.size() + ":" + status.getText());
+                if (tweets.size() > 100) {
+                    synchronized (lock) {
+                        lock.notify();
+                    }
+                    System.out.println("unlocked");
                 }
             }
+
+            @Override
+            public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+            }
+        };
+
+        TwitterStream twitterStream = new TwitterStreamFactory(cb.build()).getInstance();
+
+        twitterStream.addListener(listener);
+
+        twitterStream.sample("en");
+
+        try {
+            synchronized (lock) {
+                lock.wait();
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        //persist to the accessToken for future reference.
-        //storeAccessToken(twitter.verifyCredentials().getId() , accessToken);
 
-        return twitter;
-    }
+        for (Status tweet : tweets) {
+            System.out.println(tweet);
+        }
 
-    private static void crawlTwitter(Twitter twitter) throws Exception{
-
-    }
-
-    public static void main(String args[]) throws Exception{
-        Twitter twitter = getAccessToken();
-        //TODO program has keys, now crawl
-        crawlTwitter(twitter);
 
         System.exit(0);
     }
