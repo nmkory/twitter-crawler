accff2c217dd86ba888f78b4fe68e82f612122b3
Nicholas Kory <nmkory@ucdavis.edu>
2020-05-31 15:39:09 -0700

    Adding comments and non-null checks for safe execution

--- LuceneBuilder/src/main/java/LuceneBuilder.java
+++ LuceneBuilder/src/main/java/LuceneBuilder.java
@@ -12,38 +12,63 @@ import org.json.simple.parser.ParseException;
 import java.io.*;
 import java.util.ArrayList;
 
+import static java.util.Objects.*;
+
 public class LuceneBuilder {
+    //Default directory for the index from LuceneBuilder class path
     static final String INDEX_DIR = "../index";
+    //Default directory for the json files from LuceneBuilder class path
     static final String JSON_DIR = "../Crawler";
 
+    //Non static variables that should be accessed through instance of this class only
     String indexDir;
     String JSONdir;
     IndexWriter indexWriter = null;
 
-    public LuceneBuilder(String indexDir, String JSONdir) {
-        this.indexDir = indexDir;
-        this.JSONdir = JSONdir;
-    }
-
+    /**
+     * Default constructor
+     */
     public LuceneBuilder() {
         this.indexDir = INDEX_DIR;
         this.JSONdir = JSON_DIR;
-    }
+    }  //LuceneBuilder()
 
+    /**
+     * Parameterized constructor
+     * @param indexDir the directory where we will store the Lucene index
+     * @param JSONdir the directory where we will get the JSON files farmed from Twitter
+     */
+    public LuceneBuilder(String indexDir, String JSONdir) {
+        this.indexDir = indexDir;
+        this.JSONdir = JSONdir;
+    }  //LuceneBuilder(String indexDir, String JSONdir)
+
+
+    /**
+     * getIndexWriter() creates or opens the index for writing
+     * @param indexDir the directory where will will create or open the index
+     * @return the IndexWriter for our Lucene index
+     * @throws IOException when directory open is bad
+     */
     public IndexWriter getIndexWriter(String indexDir) throws IOException {
         //Directory indexDir = new RAMDirectory(); //use to put directory in RAM
         Directory dir = FSDirectory.open(new File(indexDir).toPath());
         IndexWriterConfig luceneConfig = new IndexWriterConfig(new StandardAnalyzer());
 
         return(new IndexWriter(dir, luceneConfig));
-    }
+    }  //getIndexWriter()
 
-
+    /**
+     * parseJSONFiles() Opens the JSON files in the directory provided and adds them to the JSON Array List
+     * @param JSONdir the directory where the JSON files are located
+     * @return an Array List of JSON Objects of Tweets
+     * @throws IOException when opening the file directory goes bad
+     */
     public ArrayList <JSONObject> parseJSONFiles(String JSONdir) throws IOException {
         BufferedReader br;
         String jsonString;
         JSONParser parser = new JSONParser();
-        ArrayList <JSONObject> jsonArrayList = new ArrayList<JSONObject>();
+        ArrayList <JSONObject> jsonArrayList = new ArrayList<>();
 
         //Go the Crawler directory
         File dir = new File(JSONdir);
@@ -52,7 +77,7 @@ public class LuceneBuilder {
         File[] jsonFiles = dir.listFiles((dir1, filename) -> filename.endsWith(".json"));
 
         //For each new line delimited json file in the file directory
-        for (File jsonFile : jsonFiles) {
+        for (File jsonFile : requireNonNull(jsonFiles)) {
 
             //Set the buffered reader
             br = new BufferedReader(new FileReader(jsonFile.getCanonicalPath()));
@@ -76,10 +101,11 @@ public class LuceneBuilder {
     public Document buildDocument(JSONObject jsonObject) {
         Document doc = new Document();
 
+        // Add as text field so the text is searchable, per TA
         doc.add(new TextField("text",(String) jsonObject.get("Text"), Field.Store.NO));
         doc.add(new StringField("user",(String) jsonObject.get("User"), Field.Store.YES));
         doc.add(new StringField("datetime",(String) jsonObject.get("Datetime"), Field.Store.YES));
-        // Add as doc values field, so we can compute range facets
+        // Add as doc values field so we can compute range facets
         doc.add(new NumericDocValuesField("timestamp", (long) jsonObject.get("Timestamp")));
         // Add as numeric field so we can drill-down
         doc.add(new LongPoint("timestamp", (long) jsonObject.get("Timestamp")));
@@ -102,14 +128,14 @@ public class LuceneBuilder {
         }
     }
 
-    public void buildIndex() throws IOException, ParseException {
-        IndexWriter indexWriter = getIndexWriter(indexDir);
+    public void buildIndex() throws IOException {
+        indexWriter = getIndexWriter(indexDir);
         ArrayList <JSONObject> jsonArrayList = parseJSONFiles(JSONdir);
         indexTweets(jsonArrayList, indexWriter);
         indexWriter.close();
     }
 
-    public static void main(String args[]) throws IOException, ParseException {
+    public static void main(String[] args) throws IOException {
         LuceneBuilder luceneIndex = new LuceneBuilder();
         luceneIndex.buildIndex();
     }
